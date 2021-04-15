fe9ef1833f02db3d060aefe4cbe1a558b31b42cc
Alexandru <cojuharialexandru31@gmail.com>
2021-04-16 00:51:27 +0300

    Reducer was moved to a separated file

---
+++ src/reducers/reducer.js
@@ -0,0 +1,77 @@
+const piecesPositionReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_HISTORY':
+      return {
+        ...state,
+        movesHistory: state.movesHistory.concat({
+          id: action.id,
+          pieceName: action.pieceName,
+          oldPosition: action.oldPosition,
+          newPosition: action.newPosition,
+        }),
+      };
+    case 'CLEAN_UP_HISTORY':
+      return {
+        ...state,
+        movesHistory: [],
+      };
+    case 'MOVE_WHITE_QUEEN':
+      return {
+        ...state,
+        whiteQueenPosition: {
+          isOnBoard: action.isOnBoard,
+          row: action.row,
+          column: action.column,
+        },
+      };
+    case 'MOVE_WHITE_KNIGHT':
+      return {
+        ...state,
+        whiteKnightPosition: {
+          isOnBoard: action.isOnBoard,
+          row: action.row,
+          column: action.column,
+        },
+      };
+    case 'MOVE_WHITE_BISHOP':
+      return {
+        ...state,
+        whiteBishopPosition: {
+          isOnBoard: action.isOnBoard,
+          row: action.row,
+          column: action.column,
+        },
+      };
+    case 'MOVE_BLACK_QUEEN':
+      return {
+        ...state,
+        blackQueenPosition: {
+          isOnBoard: action.isOnBoard,
+          row: action.row,
+          column: action.column,
+        },
+      };
+    case 'MOVE_BLACK_KNIGHT':
+      return {
+        ...state,
+        blackKnightPosition: {
+          isOnBoard: action.isOnBoard,
+          row: action.row,
+          column: action.column,
+        },
+      };
+    case 'MOVE_BLACK_BISHOP':
+      return {
+        ...state,
+        blackBishopPosition: {
+          isOnBoard: action.isOnBoard,
+          row: action.row,
+          column: action.column,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export { piecesPositionReducer };
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { createStore, compose } from 'redux';
+import { piecesPositionReducer } from '../reducers/reducer.js';
 
 class PiecePosition {
   constructor(isOnBoard, row, column) {
@@ -11,82 +12,6 @@ class PiecePosition {
 const finalCreateStore = compose(
   window.devToolsExtension ? window.devToolsExtension() : (f) => f
 )(createStore);
-
-const piecesPositionReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TO_HISTORY':
-      return {
-        ...state,
-        movesHistory: state.movesHistory.concat({
-          id: action.id,
-          pieceName: action.pieceName,
-          oldPosition: action.oldPosition,
-          newPosition: action.newPosition,
-        }),
-      };
-    case 'CLEAN_UP_HISTORY':
-      return {
-        ...state,
-        movesHistory: [],
-      };
-    case 'MOVE_WHITE_QUEEN':
-      return {
-        ...state,
-        whiteQueenPosition: {
-          isOnBoard: action.isOnBoard,
-          row: action.row,
-          column: action.column,
-        },
-      };
-    case 'MOVE_WHITE_KNIGHT':
-      return {
-        ...state,
-        whiteKnightPosition: {
-          isOnBoard: action.isOnBoard,
-          row: action.row,
-          column: action.column,
-        },
-      };
-    case 'MOVE_WHITE_BISHOP':
-      return {
-        ...state,
-        whiteBishopPosition: {
-          isOnBoard: action.isOnBoard,
-          row: action.row,
-          column: action.column,
-        },
-      };
-    case 'MOVE_BLACK_QUEEN':
-      return {
-        ...state,
-        blackQueenPosition: {
-          isOnBoard: action.isOnBoard,
-          row: action.row,
-          column: action.column,
-        },
-      };
-    case 'MOVE_BLACK_KNIGHT':
-      return {
-        ...state,
-        blackKnightPosition: {
-          isOnBoard: action.isOnBoard,
-          row: action.row,
-          column: action.column,
-        },
-      };
-    case 'MOVE_BLACK_BISHOP':
-      return {
-        ...state,
-        blackBishopPosition: {
-          isOnBoard: action.isOnBoard,
-          row: action.row,
-          column: action.column,
-        },
-      };
-    default:
-      return state;
-  }
-};
 
 const store = finalCreateStore(piecesPositionReducer, {
   whiteQueenPosition: new PiecePosition(true, 1, 1),
